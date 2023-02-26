7826a4ce92d161f3d7a043c902dfb02dd2c50b7c
ceibeisfree <ceibe.free@gmail.com>
2023-02-26 23:18:42 +0100

    Create ProgramaPrincipal.java

---
+++ ifp DAW/Programación/proProyectoConceptosGenerales/src/es/ifp/programcion/conceptosgenerales/ProgramaPrincipal.java
@@ -0,0 +1,13 @@
+package es.ifp.programcion.conceptosgenerales;
+
+public class ProgramaPrincipal {
+	
+	
+	
+	//Punto de entrada de nuestra apliccion
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola este es mi primer programa JAVA");
+	}
+
+}
