087cc2a242f2ecb6c881657cbcfd1c45ec61a705
ceibeisfree <ceibe.free@gmail.com>
2023-03-02 20:30:34 +0100

    actualizacion 1.1

---
+++ ifp DAW/Programación/Eclipse/projectoProgramacionSuma/src/es/ifp/programacion/uf1/pseudocodigo/PrimerProgramaSuma.java
@@ -0,0 +1,34 @@
+package es.ifp.programacion.uf1.pseudocodigo;
+
+import java.util.Scanner;
+
+/**
+ * Clase que dados dos numeros los suma.
+ * 
+ * @author hakuna matata
+ *
+ */
+
+public class PrimerProgramaSuma {
+
+	public static void main(String[] args) {
+		
+		// Bloque eclaracin de variables
+		
+		int num1;
+		int num2;
+		int result;
+		Scanner sc = new Scanner(System.in);
+		
+		// Entrada de datos
+		
+		System.out.println("Introduzca el primer número");
+		num1 = Integer.parseInt(sc.nextLine());
+		
+		System.out.println("Introduzca el segundo número");
+		num2 = Integer.parseInt(sc.nextLine());
+		
+		
+	}
+
+}
---
+++ ifp DAW/Programación/PSeInt/suma.psc
@@ -0,0 +1,14 @@
+Algoritmo sin_titulo
+	// Este algorimto suma dos numeros
+	// Definicion de variables
+	Definir num1 como entero
+	Definir num2 como entero
+	Definir result como entero
+	// Entrada de datos
+	Escribir "Introduzca el primer numero: "
+	Leer num1
+	Escribir "Introduzca el segundo numero: "
+	Leer num2
+	result <- num1 + num2
+	Escribir "La suma de los numeros es: ", result
+FinAlgoritmo
