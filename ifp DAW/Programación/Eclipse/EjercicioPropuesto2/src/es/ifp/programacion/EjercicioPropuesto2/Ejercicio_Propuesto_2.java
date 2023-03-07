8470b343f5177fc40c9ddabb0ffea71b6b7df18e
ceibeisfree <ceibe.free@gmail.com>
2023-03-07 22:05:30 +0100

    actualizacion 2.4

---
+++ ifp DAW/Programación/Eclipse/EjercicioPropuesto2/src/es/ifp/programacion/EjercicioPropuesto2/Ejercicio_Propuesto_2.java
@@ -0,0 +1,34 @@
+package es.ifp.programacion.EjercicioPropuesto2;
+
+/**
+ * Ejercicio propuesto 2:
+Operadores y operaciones booleanas
+Crea un programa en Java que realice la comparación entre dos números indicando si el primer número introducido es mayor, menor o igual.
+
+El programa recibirá los dos números por teclado que serán introducidos por el usuario.
+
+Una vez realizada la comparación se mostrará el resultado final en la consola.
+
+Por ejemplo:
+Entrada:
+Dato1:  5
+Dato2:  6
+
+Salida: 
+¿El número 1 es menor que el número 2?: true
+¿El número 1 es igual que el número 2?: false
+¿El número 1 es mayor que el número 2?: false
+
+ * @author hakuna matata
+ *
+ */
+
+
+public class Ejercicio_Propuesto_2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ ifp DAW/Programación/Eclipse/EjercicioPropuesto2/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author hakuna matata
+ *
+ */
+module EjercicioPropuesto2 {
+}
